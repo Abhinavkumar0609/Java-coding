b6a85096cbc104097406b423f8771faa7d2dc832
Abhinav Kumar <abhinavkumar06092001@gmail.com>
2025-06-09 19:35:57 +0530

    Add files via upload

---
+++ helloworldnewline.java
@@ -0,0 +1,6 @@
+public class helloworldnewline {
+    public static void main(String args[]){
+        System.out.println("Hello world \n i am learning java");
+        System.out.println("with DSA");
+    }
+}
