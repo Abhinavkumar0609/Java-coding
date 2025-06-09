786c3d7e9856eccb4a5ce0bbbe492cf673253bb4
Abhinav Kumar <abhinavkumar06092001@gmail.com>
2025-06-09 22:16:36 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args) {
+        String name="abhinav kumar";
+        int age=23;
+        double price=50.60;
+        int a=10;
+        int b;
+    }
+}
