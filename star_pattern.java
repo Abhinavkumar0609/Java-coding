040e5fd35f5beb5d61d5a6edfe5ec08184e3714c
Abhinav Kumar <abhinavkumar06092001@gmail.com>
2025-06-09 22:16:16 +0530

    Add files via upload

---
+++ star_pattern.java
@@ -0,0 +1,8 @@
+public class star_pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
